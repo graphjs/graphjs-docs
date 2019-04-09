8534863a32d2ff0c6b71452f113db60840fc89d0
Emre Sokullu <esokullu@gmail.com>
2019-04-09 11:08:14 +0300

    Update GetBlogPosts.js

--- functions-data/Call/GetBlogPosts.js
+++ functions-data/Call/GetBlogPosts.js
@@ -3,5 +3,11 @@ export default {
     "id": "getBlogPosts",
     "parent": "blogApiFunctions",
     "summary": "A javascript call to get all blog posts programmatically. See below for a sample response. See also [startBlogPost](/docs/functions/startBlogPost), [getBlogPost](/docs/functions/getBlogPost), [ediBlogPost](/docs/functions/ediBlogPost), [removeBlogPost](/docs/functions/removeBlogPost). Part of the Blog family of tags/calls.",
-    "specs": []
+    "specs": [{
+        "type": "text",
+        "key": "order",
+        "label": "Order",
+        "placeholder": "Optional. Possible values are either \"asc\" (ascending) or \"desc\" (descending).",
+        "value": "desc"
+    }]
 }
